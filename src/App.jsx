a15a481c20d87c1a32a692602f1253bb63007364
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-13 09:35:49 -0700

    chore: add app scaffolding

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { CakeView } from './views/CakeView'
+import { IceCreamView } from './views/IceCreamView'
+import { UserView } from './views/UserView'
+
+function App() {
+  return (
+    <div className="App">
+      <CakeView />
+      <IceCreamView />
+      <UserView />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/IceCreamView.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const IceCreamView = () => {
+  return (
+    <div>IceCreamView</div>
+  )
+}
