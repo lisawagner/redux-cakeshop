fffdc2f9f2bbd36e2c8dbfd67bdecfa4fd738b6a
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-14 10:31:18 -0700

    feat: add staff 'user' listing

--- src/views/UserView.jsx
+++ src/views/UserView.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchUsers } from '../features/user/userSlice'
 
 export const UserView = () => {
+  const user = useSelector((state) => state.user)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [])
+
   return (
     <>
-      <h2>Users List</h2>
+      <h2>Staff List</h2>
+      {user.loading && <div>Loading...</div>}
+      {!user.loading && user.error ? <div>Error: {user.error}</div> : null}
+      {!user.loading && user.users.length ? (
+        <ol>
+          {
+            user.users.map(user => (
+              <li key={user.id}>{user.name}</li>
+            ))
+          }
+        </ol>
+      ) : null}
     </>
   )
 }
